31b6ec47e2a421dd41d6112cae3c638bbba19540
tanayabedase <115339691+tanayabedase@users.noreply.github.com>
2022-10-09 15:56:24 +0530

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,22 @@
+#boolean in python - True/False 
+
+from heapq import heapify
+
+
+a= 'hello world'
+print(bool(a))
+
+b= ''
+print(bool(b))
+
+c= '0'
+print(bool(c))
+
+d= []
+print(bool(d))
+
+e= [1.2345]
+print(bool(e))
+
+print(bool(1))
+print(bool(7.5))
