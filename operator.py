66cb56bcabe625a7ea22551f7d65f0e29c329622
tanayabedase <115339691+tanayabedase@users.noreply.github.com>
2022-10-31 00:34:55 +0530

    Update operator.py

--- operator.py
+++ operator.py
@@ -73,13 +73,50 @@ print(x > 4 or x > 10)
 
 print(not(x < 2 and x < 8)) #reverses the result for exa -> if the result is true it will retuen false and vice versa
 
-#5  Identity operators
+#  Identity operators
 
 a=[28,369]
 b=[28,369]
 c=a
 print(a is c) #returns true if both the variables are the same object pointing to a same memory location
 print(b is a) ##it will return false because only object is same but the memory location is different here
+
+
+a=[70,60]
+b=[70,60]
+c=a
+pirnt ( a is c)
+print(b is c) # it will return false becuase it is not a same object
+print(a==b) # it returns true because a is== b
+
+
+# is not operator returns true if it is not same object
+print ( a is not c) # it returns true as it is same object
+print( b is not c) # it returns ture as it is not same object
+print(a!=b) # it returns false as a is equals to b
+# Membership operators
+
+# These operartors are used to test if a sequnece is present or not in an object
+a=[60,70,80,90]
+print(60 in a)
+print(10 not in a)
+# Bit wise operators
+# They are used to compare Binary Numbers
+# & -> ANd <- If both bits are 1, it sets each bits to 1
+# | -> OR <- If one of two bits is 1, it sets each bits to 1
+# ^ -> XOR <- If only one of two bits is 1, it sets each bits to 1
+# # -> NOR <- It returns compliment of a bit
+# << -> Zero fill left shift -> the binary number is appended with 0's at the end
+# >> -> Right Shift <- In single term it removes elemnets from right side
+
+x= 28
+y= 7
+print(x & y)
+print(x | y)
+print(x ^ y)
+# print(x % y)
+print(x << y)
+print(x >> y)
 
 
 #Membership Operators
